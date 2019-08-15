9800b1395624ca6916b3a99f005541955c8aff43
VeromcaWang <weiqian.wang@outlook.com>
2019-08-14 19:10:25 -0500

    First commit

---
+++ src/com/weiqian/leetcodesolutions/algorithm/A1TwoSum.java
@@ -0,0 +1,26 @@
+package com.weiqian.leetcodesolutions.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
+public class A1TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int m = target - nums[i];
+            if(map.containsKey(m)) return new int[]{i, map.get(m)};
+            map.put(nums[i], i);
+        }
+
+        return null;
+    }
+}
